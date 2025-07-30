2945355bd860b3dddd51adf5450e0fc36ed4f7f8
francesco <francesco.rossettiiii@gmail.com>
2025-07-30 18:50:19 +0200

    adapt deepjanus generator for 3D roads

--- sample_test_generators/deepjanus_seed_generator.py
+++ sample_test_generators/deepjanus_seed_generator.py
@@ -3,7 +3,7 @@
 # by V. Riccio and P. Tonella
 # https://doi.org/10.1145/3368089.3409730
 
-from random import randint
+from random import randint, uniform
 from typing import List, Tuple
 
 from shapely.geometry import Point
@@ -21,7 +21,7 @@ Tuple2F = Tuple[float, float]
 
 
 def catmull_rom_spline(p0, p1, p2, p3, num_points=20):
-    """p0, p1, p2, and p3 should be (x,y) point pairs that define the Catmull-Rom spline.
+    """p0, p1, p2, and p3 should be (x,y,z) point pairs that define the Catmull-Rom spline.
     num_points is the number of points to include in this curve segment."""
     # Convert the points to numpy so that we can do array multiplication
     p0, p1, p2, p3 = map(np.array, [p0, p1, p2, p3])
@@ -30,10 +30,8 @@ def catmull_rom_spline(p0, p1, p2, p3, num_points=20):
     # For knot parametrization
     alpha = 0.5
 
-    def tj(ti, p_i, p_j):
-        xi, yi = p_i
-        xj, yj = p_j
-        return (((xj - xi) ** 2 + (yj - yi) ** 2) ** 0.5) ** alpha + ti
+    def tj(ti, pi, pj):
+        return (((np.linalg.norm(pj - pi)) ** alpha) + ti)
 
     # Knot sequence
     t0 = 0
@@ -41,13 +39,10 @@ def catmull_rom_spline(p0, p1, p2, p3, num_points=20):
     t2 = tj(t1, p1, p2)
     t3 = tj(t2, p2, p3)
 
-    # Only calculate points between p1 and p2
-    t = np.linspace(t1, t2, num_points)
+    # Interpolation points between p1 and p2
+    t = np.linspace(t1, t2, num_points).reshape(num_points, 1)
 
-    # Reshape so that we can multiply by the points p0 to p3
-    # and get a point for each value of t.
-    t = t.reshape(len(t), 1)
-
+    # Interpolation
     a1 = (t1 - t) / (t1 - t0) * p0 + (t - t0) / (t1 - t0) * p1
     a2 = (t2 - t) / (t2 - t1) * p1 + (t - t1) / (t2 - t1) * p2
     a3 = (t3 - t) / (t3 - t2) * p2 + (t - t2) / (t3 - t2) * p3
@@ -58,34 +53,30 @@ def catmull_rom_spline(p0, p1, p2, p3, num_points=20):
     c = (t2 - t) / (t2 - t1) * b1 + (t - t1) / (t2 - t1) * b2
     return c
 
-
 def catmull_rom_chain(points: List[tuple], num_spline_points=20) -> List:
     """Calculate Catmull-Rom for a chain of points and return the combined curve."""
-    # The curve cr will contain an array of (x, y) points.
+    # The curve cr will contain an array of (x, y, z) points.
     cr = []
     for i in range(len(points) - 3):
         c = catmull_rom_spline(points[i], points[i + 1], points[i + 2], points[i + 3], num_spline_points)
         if i > 0:
-            c = np.delete(c, [0], axis=0)
+            c = np.delete(c, [0], axis=0)  # Avoid duplicated point
         cr.extend(c)
     return cr
 
-
-def catmull_rom_2d(points: List[tuple], num_points=20) -> List[tuple]:
-    if len(points) < 4:
-        raise ValueError("points should have at least 4 points")
-    np_points_array = catmull_rom_chain(points, num_points)
-    return [(p[0], p[1]) for p in np_points_array]
-
-
 def catmull_rom(points: List[tuple], num_spline_points=20) -> List[tuple]:
+    """
+    Accepts points with format (x, y, z, width), returns smoothed spline
+    as list of (x, y, z, width).
+    """
     if len(points) < 4:
         raise ValueError("points should have at least 4 points")
-    assert all(x[3] == points[0][3] for x in points)
-    np_point_array = catmull_rom_chain([(p[0], p[1]) for p in points], num_spline_points)
-    z0 = points[0][2]
+    assert all(p[3] == points[0][3] for p in points), "All points must have same width"
+    
+    np_point_array = catmull_rom_chain([(p[0], p[1], p[2]) for p in points], num_spline_points)
+
     width = points[0][3]
-    return [(p[0], p[1], z0, width) for p in np_point_array]
+    return [(p[0], p[1], p[2], width) for p in np_point_array]
 
 
 class RoadGenerator:
@@ -93,6 +84,8 @@ class RoadGenerator:
 
     NUM_INITIAL_SEGMENTS_THRESHOLD = 2
     NUM_UNDO_ATTEMPTS = 20
+    MAX_Z_VARIATION = 6.0  # max variation for segment
+    MAX_SLOPE = 0.3  # max slope/distance ratio
 
     def __init__(self, num_control_nodes=15, max_angle=None, seg_length=None,
                  num_spline_nodes=None, initial_node=(125.0, 0.0, -28.0, 8.0),
@@ -108,44 +101,39 @@ class RoadGenerator:
         self.seg_length = seg_length
         self.road_bbox = RoadBoundingBox(bbox_size)
         assert not self.road_bbox.intersects_vertices(self._get_initial_point())
-        # assert self.road_bbox.intersects_sides(self._get_initial_point())
 
     def generate_control_nodes(self, attempts=NUM_UNDO_ATTEMPTS) -> List[Tuple4F]:
         condition = True
         while condition:
             nodes = [self._get_initial_control_node(), self.initial_node]
-
-            # i is the number of valid generated control nodes.
             i = 0
-
-            # When attempt >= attempts and the skeleton of the road is still invalid,
-            # the construction of the skeleton starts again from the beginning.
-            # attempt is incremented every time the skeleton is invalid.
             attempt = 0
 
             while i < self.num_control_nodes and attempt <= attempts:
                 nodes.append(self._get_next_node(nodes[-2], nodes[-1], self._get_next_max_angle(i)))
                 road_polygon = RoadPolygon.from_nodes(nodes)
-
-                # budget is the number of iterations used to attempt to add a valid next control node
-                # before also removing the previous control node.
                 budget = self.num_control_nodes - i
                 assert budget >= 1
 
                 intersect_boundary = self.road_bbox.intersects_boundary(road_polygon.polygons[-1])
-                is_valid = road_polygon.is_valid() and (
-                            ((i == 0) and intersect_boundary) or ((i > 0) and not intersect_boundary))
+                is_valid = (
+                    road_polygon.is_valid() and
+                    (((i == 0) and intersect_boundary) or ((i > 0) and not intersect_boundary)) and
+                    self._is_valid_slope(nodes[-2], nodes[-1])
+                )
+
                 while not is_valid and budget > 0:
                     nodes.pop()
                     budget -= 1
                     attempt += 1
-
                     nodes.append(self._get_next_node(nodes[-2], nodes[-1], self._get_next_max_angle(i)))
                     road_polygon = RoadPolygon.from_nodes(nodes)
-
                     intersect_boundary = self.road_bbox.intersects_boundary(road_polygon.polygons[-1])
-                    is_valid = road_polygon.is_valid() and (
-                            ((i == 0) and intersect_boundary) or ((i > 0) and not intersect_boundary))
+                    is_valid = (
+                        road_polygon.is_valid() and
+                        (((i == 0) and intersect_boundary) or ((i > 0) and not intersect_boundary)) and
+                        self._is_valid_slope(nodes[-2], nodes[-1])
+                    )
 
                 if is_valid:
                     i += 1
@@ -159,7 +147,6 @@ class RoadGenerator:
                 assert RoadPolygon.from_nodes(nodes).is_valid()
                 assert 0 <= i <= self.num_control_nodes
 
-            # The road generation ends when there are the control nodes plus the two extra nodes needed by the current Catmull-Rom model
             if len(nodes) - 2 == self.num_control_nodes:
                 condition = False
         return nodes
@@ -174,34 +161,38 @@ class RoadGenerator:
         while condition:
             control_nodes = self.generate_control_nodes()
             control_nodes = control_nodes[1:]
+            control_nodes[1] = (control_nodes[1][0], control_nodes[1][1], control_nodes[0][2], control_nodes[1][3])
             sample_nodes = catmull_rom(control_nodes, self.num_spline_nodes)
             if self.is_valid(control_nodes, sample_nodes, self.num_spline_nodes):
                 condition = False
 
-        road = [(node[0], node[1]) for node in sample_nodes]
+        road = [(node[0], node[1], node[2]) for node in sample_nodes]
         return road
 
     def _get_initial_point(self) -> Point:
         return Point(self.initial_node[0], self.initial_node[1])
 
     def _get_initial_control_node(self) -> Tuple4F:
-        x0, y0, z, width = self.initial_node
+        x0, y0, z0, width = self.initial_node
         x, y = self._get_next_xy(x0, y0, 270)
-        assert not (self.road_bbox.bbox.contains(Point(x, y)))
-
+        z = self._get_next_z(z0)
         return x, y, z, width
 
     def _get_next_node(self, first_node, second_node: Tuple4F, max_angle) -> Tuple4F:
-        v = np.subtract(second_node, first_node)
+        v = np.subtract(second_node[:2], first_node[:2])
         start_angle = int(np.degrees(np.arctan2(v[1], v[0])))
         angle = randint(start_angle - max_angle, start_angle + max_angle)
         x0, y0, z0, width0 = second_node
         x1, y1 = self._get_next_xy(x0, y0, angle)
-        return x1, y1, z0, width0
+        z1 = self._get_next_z(z0)
+        return x1, y1, z1, width0
 
     def _get_next_xy(self, x0: float, y0: float, angle: float) -> Tuple2F:
         angle_rad = math.radians(angle)
         return x0 + self.seg_length * math.cos(angle_rad), y0 + self.seg_length * math.sin(angle_rad)
+
+    def _get_next_z(self, z0: float) -> float:
+        return max((z0 + uniform(-self.MAX_Z_VARIATION, self.MAX_Z_VARIATION)), -28.0)
 
     def _get_next_max_angle(self, i: int, threshold=NUM_INITIAL_SEGMENTS_THRESHOLD) -> float:
         if i < threshold or i == self.num_control_nodes - 1:
@@ -209,6 +200,12 @@ class RoadGenerator:
         else:
             return self.max_angle
 
+    def _is_valid_slope(self, prev_node, new_node) -> bool:
+        dz = abs(new_node[2] - prev_node[2])
+        dx = math.hypot(new_node[0] - prev_node[0], new_node[1] - prev_node[1])
+        if dx == 0:
+            return False
+        return (dz / dx) <= self.MAX_SLOPE
 
 class JanusGenerator():
     def __init__(self, executor=None, map_size=None):
